f182f790fbad332e2b7b74f9ab2e92ee98fb56f3
AN EUNSOL <dev.eunsol@gmail.com>
2022-07-27 02:52:49 +0900

    fix: Service annotation 삭제

--- src/main/java/com/naver/kin/service/UsersService.java
+++ src/main/java/com/naver/kin/service/UsersService.java
@@ -2,12 +2,9 @@ package com.naver.kin.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.naver.kin.domain.PagingVO;
 import com.naver.kin.domain.UsersVO;
 
-@Service
 public interface UsersService {
 	int register(UsersVO uvo);
 	UsersVO login(UsersVO uvo);
